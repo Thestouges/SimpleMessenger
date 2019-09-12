938da5f41defded6db48ef5e9385488cc394e529
Thestouges <thestouges@gmail.com>
2019-09-11 20:00:43 -0400

    added database schema

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -1,1 +1,13 @@
-﻿test
+﻿CREATE TABLE [dbo].[UserAccount](
+	[User] [nvarchar](30) NOT NULL,
+	[Password] [nvarchar](max) NOT NULL,
+	[LoggedIn] [nvarchar](1) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[UserMessage](
+	[User] [nvarchar](30) NULL,
+	[Message1] [nvarchar](max) NULL,
+	[timestamp] [timestamp] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
